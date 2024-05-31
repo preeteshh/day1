6de7013106bcf24a75f8da77b1aa78009d75f2b0
preeteshh <preeteshbiswas02@gmail.com>
2024-05-31 19:30:43 +0000

    DAY01

---
+++ .devcontainer/basics/lecct4.js
@@ -0,0 +1,8 @@
+const accountId=23432
+let accountEmail="preetesh@gmail.com"
+var password="12345"
+accountCity="jaipur"
+
+accountId=234 
+
+console.log(accountId);
---
+++ .devcontainer/basics/lect5.js
@@ -0,0 +1,15 @@
+"use strict"//this will treat all the JS code to newer versions
+// alert(3+3) we are using nodejs not website
+//DATA TYPES
+let age=18
+let loggedin=false
+let name="preetesh"
+// number me range hota hain 2pow53
+// bigint is used in trading and reddit like apps to handel huge calculations
+//  symbol is used to find the uniqueness 
+
+
+console.log(typeof "propree");
+console.log(typeof null)           //output is object
+console.log(typeof undefined)         //output is undefined
+
